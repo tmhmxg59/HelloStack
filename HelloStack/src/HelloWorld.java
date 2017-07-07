1f53df2484fb3e85de27f03ef7ef3d7a04421510
tmhmxg59 <mgreenwood@houstonmethodist.org>
2017-07-07 11:45:58 -0500

    Add files via upload

---
+++ HelloStack/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mikegreenwood on 7/7/17.
+ */
+
+/*
+    This is the format for a multiline comment
+    It is best practice to use this format if the comment has mutliple lines
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
